053b048219997b5b9639f53e8b6af7f19f415898
Ayush <ayushpadaruth2019@gmail.com>
2025-09-21 13:18:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,12 +17,17 @@ function getResponse() {
     reply = "I was created by Ayush Padaruth, the legend himself! 🚀";
   } else if (input.includes("joke")) {
     reply = "Why did the computer show up at work late? It had a hard drive! 😆";
-  } else {
-    reply = "Hmm 🤔 I don’t know that yet. Ayush can teach me more!";
-  }
+  } else if (input.includes("do you know joshua martin?")) {
+    reply = "Ah, Joshua Martin? Of course I know him! That guy is basically a legend in the F1 world of everyday life. 🏎️💨 He probably treats his breakfast like a pit stop, his homework like a Grand Prix, and his weekend like the championship race itself. Honestly, if he ever got behind the wheel of an actual F1 car, I’d bet he’d try to drift around every corner while cheering for his favorite driver at the same time! 😂";
+}
+
+  else {
+    reply = "Hmm 🤔 I don’t know that yet. Ayush still needs to teach me more!";
+  } 
 
   responseBox.innerHTML = "🤖 Ayush's AI says: " + reply;
 }
+
 
 
 
