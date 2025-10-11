9af248161443527f8d07acfcc95cad500ba7642e
Ayush <ayushpadaruth2019@gmail.com>
2025-10-11 14:15:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ async function signup() {
   }
 
   try {
-    const res = await fetch("https://https://c9c8428f-7614-4a94-a4a6-b7ca87e60153-00-1z22thnwna9oh.riker.replit.dev/signup", {
+    const res = await fetch("https://c9c8428f-7614-4a94-a4a6-b7ca87e60153-00-1z22thnwna9oh.riker.replit.dev/signup", {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({username,email,password})
@@ -100,7 +100,7 @@ async function login() {
   }
 
   try {
-    const res = await fetch("https://https://c9c8428f-7614-4a94-a4a6-b7ca87e60153-00-1z22thnwna9oh.riker.replit.dev/login", {
+    const res = await fetch("https://c9c8428f-7614-4a94-a4a6-b7ca87e60153-00-1z22thnwna9oh.riker.replit.dev/login", {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({username,password})
@@ -129,5 +129,6 @@ function logout(){
   localStorage.removeItem("username");
   location.reload();
 }
+
 
 
