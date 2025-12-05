7d22ba20f8d64745ccbe7aa3f563501a517870a4
Ayush <ayushpadaruth2019@gmail.com>
2025-12-05 19:17:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,6 @@ let currentUserId = null;
 // INTRO SCREEN
 // ========================
 window.addEventListener("load", () => {
-    fetch("/welcome-message")
-        .then(res => res.json())
-        .then(data => {
-            if (data.welcome) {
-                addMessage(data.welcome, "ai"); // show as AI bubble
-            }
-        });
-});
   const intro = document.getElementById("intro");
   setTimeout(() => {
     intro.style.opacity = "0";
@@ -280,6 +272,7 @@ async function loadPreviousMessages() {
     console.error("⚠️ Could not load previous messages.", err);
   }
 }
+
 
 
 
